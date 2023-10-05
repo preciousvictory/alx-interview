b3b6608d503f0f427360bb8da597558ed5582261
Victory Abiodun-Omoniyi <victoryexcel2018@gmail.com>
2023-10-05 06:06:12 -0700

    lockboxex

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -37,6 +37,11 @@ def canUnlockAll(boxes):
                         if j not in unlocked:
                             unlocked.append(j)
 
-    if len(unlocked) == len(boxes):
-        return True
-    return False
+    check_unlocked = False
+    for i in range(len(boxes)):
+        if i not in unlocked:
+            check_unlocked = False
+        else:
+            check_unlocked = True
+
+    return check_unlocked
