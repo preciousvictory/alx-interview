2780f589a80f699399e12787e8af42baec09af88
Victory Abiodun-Omoniyi <victoryexcel2018@gmail.com>
2023-10-10 01:49:20 -0700

    minoperations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,25 +4,24 @@
 
 
 def minOperations(n):
-    '''minOperations
-    fewest number of operations needed to result in exactly n H characters
+    '''minOperations fewest number of operations needed to result
+    in exactly n H characters
     '''
     if not isinstance(n, int):
         return 0
-
     text_file = 1
     count = 0
     copy = 0
     while text_file < n:
-        '''start copy (initialize)'''
+        # start copy (initialize)
         if copy == 0:
             copy = text_file
             text_file += copy
             count += 2
+        '''if the number of characters in the file remaining to reach exactly
+        n characters and if number of characters remaining divisible by the
+        number of characters already in the file
         '''
-        if the number of characters in the file remaining to reach exactly n
-        characters and if number of characters remaining divisible by the numbe
-        of characters already in the file'''
         elif n - text_file > 0 and (n - text_file) % text_file == 0:
             copy = text_file
             text_file += copy
