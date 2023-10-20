0731348e0f2db890c4ee2ed449cbbd5c4de75e30
Victory Abiodun-Omoniyi <victoryexcel2018@gmail.com>
2023-10-19 20:21:00 -0700

    log parsing

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""0-stats
+a script that reads stdin line by line and computes metrics
+"""
+import sys
+import ipaddress
+
+
+def print_stats(filesize, code_count):
+    print(f'File size: {filesize}')
+    for code, value in code_count.items():
+        if value > 0:
+            print(f'{code}: {value}')
+
+
+if __name__ == '__main__':
+    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    code_count = {x: 0 for x in codes}
+    count, filesize = 0, 0
+    
+    
+    try:
+        for line in sys.stdin:
+            count += 1
+            if 'q' == line.rstrip():
+                break
+
+            data = line.split(" ")
+            if len(data) != 9:
+                continue
+
+            status_code = data[-2]
+            if status_code in codes:
+                code_count[status_code] += 1
+            filesize += int(data[-1])
+
+            if count % 10 == 0:
+                print_stats(filesize, code_count)
+    except KeyboardInterrupt:
+        print_stats(filesize, code_count)
+        raise
