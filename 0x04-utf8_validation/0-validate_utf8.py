092632901ab6b5d5d18200f4ff44bb085517be04
Victory Abiodun-Omoniyi <victoryexcel2018@gmail.com>
2023-10-24 05:29:58 -0700

    validate UTF-8 encoding

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -23,32 +23,34 @@ def validUTF8(data):
                 return False
             else:
                 L = 1
-                if data[i + L] >> 6 != 10:
+                if data[i + L] >> 6 != 0b10:
                     return False
                 else:
                     valid = True
         # Byte 2 where first code point is U+0800 and last is U+FFFF
-        elif data[i] >= 0x0800 and data[i] <= 0xFFFF:
+        elif data[i] & 0b11110000 == 0b11100000:
             if data[i] >> 4 != 1110:
                 return False
             else:
                 L = 2
                 for i in L:
-                    if data[i + L] >> 6 != 10:
+                    if data[i + L] >> 6 != 0b10:
                         return False
                     else:
                         valid = True
                 i += L
         # Byte 4 - first code point is U+10000 and last is U+10FFFF
-        elif data[i] >= 0x10000 and data[i] <= 0x10FFFF:
-            if data[i] >> 3 != 11110:
+        elif data[i] & 0b11111000 == 0b11110000:
+            if data[i] >> 3 != 0b11110:
                 return False
             else:
                 L = 3
                 for i in L:
-                    if data[i + L] >> 6 != 10:
+                    if data[i + L] >> 6 != 0b10:
                         return False
                     else:
                         valid = True
                 i += L
+        else:
+            return False
     return valid
