874da49b624ea4835f32407053ff59d6b9c34dfc
Victory Abiodun-Omoniyi <victoryexcel2018@gmail.com>
2023-12-26 08:14:56 -0800

    starwars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,25 +1,20 @@
 #!/usr/bin/node
-const request = require('request');
-const filmID = process.argv[2];
+const util = require('util');
+const request = util.promisify(require('request'));
+const API_URL = 'https://swapi-api.hbtn.io/api';
 
-if (process.argv.length > 2) {
-  request(`'https://swapi-api.hbtn.io/api'/films/${filmID}/`, (err, _, body) => {
-    if (err) {
-      console.log(err);
-    }
-    const charactersURL = JSON.parse(body).characters;
-    const charactersName = charactersURL.map(
-      url => new Promise((resolve, reject) => {
-        request(url, (promiseErr, __, charactersReqBody) => {
-          if (promiseErr) {
-            reject(promiseErr);
-          }
-          resolve(JSON.parse(charactersReqBody).name);
-        });
-      }));
+async function starwarsCharacters (filmId) {
+  const endpoint = `${API_URL}/films/${process.argv[2]}/`
+  let response = await (await request(endpoint)).body;
+  response = JSON.parse(response);
+  const characters = response.characters;
 
-    Promise.all(charactersName)
-      .then(names => console.log(names.join('\n')))
-      .catch(allErr => console.log(allErr));
-  });
+  for (let i = 0; i < characters.length; i++) {
+    const urlCharacter = characters[i];
+    let character = await (await request(urlCharacter)).body;
+    character = JSON.parse(character);
+    console.log(character.name);
+  }
 }
+
+starwarsCharacters(filmID);
