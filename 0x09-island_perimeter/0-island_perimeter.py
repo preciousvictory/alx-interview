cc9d983ea90d4890e8cf5fd69469721628757e70
Victory Abiodun-Omoniyi <victoryexcel2018@gmail.com>
2023-12-29 06:05:13 +0000

    island perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""
+Island Perimeter
+"""
+
+def island_perimeter(grid):
+    """
+    returns the perimeter of the island described in grid
+    grid is a list of list of integers:
+    - 0 represents water
+    - 1 represents land
+    Each cell is square, with a side length of 1
+    Cells are connected horizontally/vertically (not diagonally).
+    grid is rectangular, with its width and height not exceeding 100
+    The grid is completely surrounded by water
+    There is only one island (or nothing).
+    The island doesn’t have “lakes” (water inside that isn’t connected to
+    the water surrounding the island).
+
+    Args:
+        grid (list of list of int): the grid representing the island
+
+    Returns:
+        int: the perimeter of the island
+    """
+
+    rows = len(grid)
+    cols = len(grid[0])
+
+    perimeter = 0
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                # check if it is at extreme edge
+                if i == 0:
+                    perimeter += 1
+                if i == rows - 1:
+                    perimeter += 1
+                if j == 0:
+                    perimeter += 1
+                if j == cols - 1:
+                    perimeter += 1
+
+                #check top
+                if  grid[i - 1][j] == 0:
+                    perimeter += 1
+                # check bottom
+                if grid[i + 1][j] == 0:
+                    perimeter += 1
+                # check left
+                if grid[i][j - 1] == 0:
+                    perimeter += 1
+                # check right
+                if grid[i][j + 1] == 0:
+                    perimeter += 1
+
+    return perimeter
